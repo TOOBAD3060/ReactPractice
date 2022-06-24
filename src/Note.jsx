87f3e68a993f2ce48291e78fd34e463bd9e2fbe0
Olalere Babatunde <71438666+TOOBAD3060@users.noreply.github.com>
2022-06-24 14:42:10 +0100

    Initial commit

---
+++ src/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h3> First Note </h3>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam beatae
+        iusto debitis!{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Note;
